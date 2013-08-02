84c8e0701da1cef3822212be74aece258d44bf6a
cgeek <cem.moreau@gmail.com>
2013-08-02 10:27:52 +0200

    Add amendments

---
+++ core/amendment.js
@@ -0,0 +1,260 @@
+var async    = require('async');
+var sha1     = require('sha1');
+var _        = require('underscore');
+var fs       = require('fs');
+
+module.exports = function Amendment(){
+
+  this.version = null;
+  this.currency = null;
+  this.number = null;
+  this.previousHash = null;
+  this.dividend = null;
+  this.coinMinPower = null;
+  this.votersRoot = null;
+  this.votersCount = null;
+  this.votersChanges = [];
+  this.membersRoot = null;
+  this.membersCount = null;
+  this.membersChanges = [];
+  this.hash = null;
+
+  this.parse = function(rawAmend, callback) {
+    if(!rawAmend){
+      callback("No amendment given");
+      return;
+    }
+    this.hash = sha1(unix2dos(rawAmend)).toUpperCase();
+    var obj = this;
+    var captures = [
+      {prop: "version",         regexp: /Version: (.*)/},
+      {prop: "currency",        regexp: /Currency: (.*)/},
+      {prop: "number",          regexp: /Number: (.*)/},
+      {prop: "previousHash",    regexp: /PreviousHash: (.*)/},
+      {prop: "dividend",        regexp: /UniversalDividend: (.*)/},
+      {prop: "coinMinPower",    regexp: /CoinMinimalPower: (.*)/},
+      {prop: "votersRoot",      regexp: /VotersRoot: (.*)/},
+      {prop: "votersCount",     regexp: /VotersCount: (.*)/},
+      {prop: "votersChanges",   regexp: /VotersChanges:\n([\s\S]*)MembersRoot/},
+      {prop: "membersRoot",     regexp: /MembersRoot: (.*)/},
+      {prop: "membersCount",    regexp: /MembersCount: (.*)/},
+      {prop: "membersChanges",  regexp: /MembersChanges:\n([\s\S]*)/}
+    ];
+    var crlfCleaned = rawAmend.replace(/\r\n/g, "\n");
+    if(crlfCleaned.match(/\n$/)){
+      async.forEach(captures, function (cap, done) {
+        if(cap.prop != "membersChanges" && cap.prop != "votersChanges")
+          simpleLineExtraction(obj, crlfCleaned, cap, done);
+        else
+          multipleLinesExtraction(obj, crlfCleaned, cap, done);
+      }, callback);
+    }
+    else callback("Bad document structure: no new line character at the end of the document.");
+  };
+
+  this.verify = function(currency, done){
+    var obj = this;
+    async.waterfall([
+      function(callback, err){
+        // Version
+        if(!obj.version || !obj.version.match(/^1$/))
+          err = {code: 100, message: "Version unknown"};
+        callback(err);
+      },
+      function(callback, err){
+        // Currency
+        if(!obj.currency || !obj.currency.match("^"+ currency + "$"))
+          err = {code: 101, message: "Currency '"+ obj.currency +"' not managed"};
+        callback(err);
+      },
+      function(callback, err){
+        // Number
+        if(!obj.number || !obj.number.match(/^\d+$/))
+          err = {code: 102, message: "Incorrect Number field"};
+        callback(err);
+      },
+      function(callback, err){
+        // Previous hash
+        var isRoot = parseInt(obj.number, 10) === 0;
+        if(!isRoot && (!obj.previousHash || !obj.previousHash.match(/^[A-Z\d]{40}$/)))
+          err = {code: 103, message: "PreviousHash must be provided for non-root amendment and match an uppercase SHA1 hash"};
+        else if(isRoot && obj.previousHash)
+          err = {code: 104, message: "PreviousHash must not be provided for root amendment"};
+        callback(err);
+      },
+      function(callback, err){
+        // Universal Dividend
+        if(obj.dividend && !obj.dividend.match(/^\d+$/))
+          err = {code: 105, message: "UniversalDividend must be a decimal number"};
+        callback(err);
+      },
+      function(callback, err){
+        // Coin Minimal Power
+        if(obj.coinMinPower && !obj.dividend)
+          err = {code: 106, message: "CoinMinimalPower requires a valued UniversalDividend field"};
+        else if(obj.coinMinPower && !obj.coinMinPower.match(/^\d+$/))
+          err = {code: 107, message: "CoinMinimalPower must be a decimal number"};
+        else if(obj.coinMinPower && obj.dividend.length < parseInt(obj.coinMinPower, 10) + 1)
+          err = {code: 108, message: "No coin can be created with this value of CoinMinimalPower and UniversalDividend"};
+        callback(err);
+      },
+      function(callback, err){
+        // VotersRoot
+        if(!obj.votersRoot || !obj.votersRoot.match(/^[A-Z\d]{40}$/))
+          err = {code: 109, message: "VotersRoot must be provided and match an uppercase SHA1 hash"};
+        callback(err);
+      },
+      function(callback, err){
+        // VotersCount
+        if(!obj.votersCount || !obj.votersCount.match(/^\d+$/))
+          err = {code: 110, message: "VotersCount must be a positive or null decimal number"};
+        callback(err);
+      },
+      function(callback, err){
+        // MembersRoot
+        if(!obj.membersRoot || !obj.membersRoot.match(/^[A-Z\d]{40}$/))
+          err = {code: 111, message: "MembersRoot must be provided and match an uppercase SHA1 hash"};
+        callback(err);
+      },
+      function(callback, err){
+        // MembersCount
+        if(!obj.membersCount || !obj.membersCount.match(/^\d+$/))
+          err = {code: 112, message: "MembersCount must be a positive or null decimal number"};
+        callback(err);
+      }
+    ], function (err, result) {
+      if(err)
+        done(err.message, err.code);
+      else
+        done();
+    });};
+
+  this.getNewMembers = function() {
+    var members = [];
+    for (var i = 0; i < this.membersChanges.length; i++) {
+      var matches = this.membersChanges[i].match(/^\+([\w\d]{40})$/);
+      if(matches){
+        members.push(matches[1]);
+      }
+    }
+    return members;
+  };
+
+  this.getLeavingMembers = function() {
+    var members = [];
+    for (var i = 0; i < this.membersChanges.length; i++) {
+      var matches = this.membersChanges[i].match(/^\-([\w\d]{40})$/);
+      if(matches){
+        members.push(matches[1]);
+      }
+    }
+    return members;
+  };
+
+  this.getNewVoters = function() {
+    var voters = [];
+    for (var i = 0; i < this.votersChanges.length; i++) {
+      var matches = this.votersChanges[i].match(/^\+([\w\d]{40})$/);
+      if(matches){
+        voters.push(matches[1]);
+      }
+    }
+    return voters;
+  };
+
+  this.getLeavingVoters = function() {
+    var voters = [];
+    for (var i = 0; i < this.votersChanges.length; i++) {
+      var matches = this.votersChanges[i].match(/^\-([\w\d]{40})$/);
+      if(matches){
+        voters.push(matches[1]);
+      }
+    }
+    return voters;
+  };
+
+  this.getRaw = function() {
+    var raw = "";
+    raw += "Version: " + this.version + "\n";
+    raw += "Currency: " + this.currency + "\n";
+    raw += "Number: " + this.number + "\n";
+    if(this.previousHash){
+      raw += "PreviousHash: " + this.previousHash + "\n";
+    }
+    if(this.dividend){
+      raw += "UniversalDividend: " + this.dividend + "\n";
+    }
+    if(this.coinMinPower){
+      raw += "CoinMinimalPower: " + this.coinMinPower + "\n";
+    }
+    raw += "VotersRoot: " + this.votersRoot + "\n";
+    raw += "VotersCount: " + this.votersCount + "\n";
+    raw += "VotersChanges:\n";
+    for(var j = 0; j < this.votersChanges.length; j++){
+      raw += this.votersChanges[j] + "\n";
+    }
+    raw += "MembersRoot: " + this.membersRoot + "\n";
+    raw += "MembersCount: " + this.membersCount + "\n";
+    raw += "MembersChanges:\n";
+    for(var i = 0; i < this.membersChanges.length; i++){
+      raw += this.membersChanges[i] + "\n";
+    }
+    return unix2dos(raw);
+  };
+
+  this.loadFromFile = function(file, done) {
+    var obj = this;
+    fs.readFile(file, {encoding: "utf8"}, function (err, data) {
+      obj.parse(data, function(err) {
+        done(err);
+      });
+    });
+  }
+};
+
+
+
+function simpleLineExtraction(am, wholeAmend, cap, done) {
+  var fieldValue = wholeAmend.match(cap.regexp);
+  if(fieldValue && fieldValue.length === 2){
+    am[cap.prop] = fieldValue[1];
+    done();
+  }
+  else done();
+}
+
+function multipleLinesExtraction(am, wholeAmend, cap, done) {
+  var fieldValue = wholeAmend.match(cap.regexp);
+  am[cap.prop] = [];
+  if(fieldValue && fieldValue.length == 2){
+    var lines = fieldValue[1].split(/\n/);
+    if(lines[lines.length - 1].match(/^$/)){
+      for (var i = 0; i < lines.length - 1; i++) {
+        var line = lines[i];
+        var fprChange = line.match(/([+-][A-Z\d]{40})/);
+        if(fprChange && fprChange.length == 2){
+          am[cap.prop].push(fprChange[1]);
+        }
+        else{
+          done("Wrong structure for line: '" + line + "'");
+          return;
+        }
+      }
+      done();
+    }
+    else done("Multiple line field '" + cap.prop + "' must end with a new line character");
+  }
+  else done();
+}
+
+function trim(str){
+  return str.replace(/^\s+|\s+$/g, '');
+}
+
+function unix2dos(str){
+  return dos2unix(str).replace(/\n/g, '\r\n');
+}
+
+function dos2unix(str){
+  return str.replace(/\r\n/g, '\n');
+}
