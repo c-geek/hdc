f75d0de2875cfcfb4e7cdf2beb40d69966112008
cgeek <cem.moreau@gmail.com>
2013-08-13 00:27:15 +0200

    v0.0.8

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var amendment   = require('./core/amendment');
 var certificate = require('./core/certificate');
+var membership  = require('./core/membership');
 
 module.exports.Amendment = amendment;
 module.exports.Certificate = certificate;
+module.exports.Membership = membership;
