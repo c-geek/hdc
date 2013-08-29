887302237aae86a2e73d49df273f5cd8034191bc
cgeek <cem.moreau@gmail.com>
2013-08-29 21:43:28 +0200

    Forgotten to export transactions part

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var amendment   = require('./core/amendment');
 var certificate = require('./core/certificate');
 var membership  = require('./core/membership');
+var transaction = require('./core/transaction');
 
 module.exports.Amendment = amendment;
 module.exports.Certificate = certificate;
 module.exports.Membership = membership;
+module.exports.Transaction = transaction;
